95b5095f269602274c1790d14a3dc2f5e6a2769d
Maxime Jaspard <maxime@ecgs.lu>
2024-06-19 08:54:19 +0200

    Add .gitignore and ignore python env

--- input_shape.py
+++ input_shape.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 
-#Define volcano shape
+#Define volcano shape Nyigo
 
 Rbase = 2500
 Zvolc = 3460
 Zbase = 2500
 
 
+#Define volcano shape Nyam
+
+Rbase = 4500
+Zvolc = 3000
+Zbase = 2250
